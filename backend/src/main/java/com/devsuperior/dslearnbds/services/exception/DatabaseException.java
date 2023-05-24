2b4cf9377d08e5d6d257fe4e835542c12ea23b73
kel2203 <raquelsb2001@gmail.com>
2023-05-24 14:51:52 -0300

    Exeptions, validation, security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exception/DatabaseException.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearnbds.services.exception;
+
+public class DatabaseException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+
+}
